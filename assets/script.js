6da8e688a71b98fcabda9f1de54f0adfb10059ca
sbeou13 <design@walf.com.br>
2023-01-02 21:40:27 -0300

    Add Mavigation arrow

--- assets/script.js
+++ assets/script.js
@@ -16,12 +16,46 @@ const slides = [
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ];
+const dots = document.getElementById("dots");
+for (let i = 0; i < slides.length; i++) {
+	dots.innerHTML += "<li class='dot'></li>";
+	const dot = document.createElement("li");
+      dot.classList.add("dot");
+}
+const dot = document.getElementsByClassName('dot');
+let i = 0;
+dot[i].classList.add("dot_selected");
+function sliderImg(i) {
+	document.querySelector("#banner > img").src=`./assets/images/slideshow/${slides[i].image}`;
+	document.querySelector("#banner > p").innerHTML = slides[i].tagLine;
+	if (dot[i]) {
+		dot[i].classList.add("dot_selected");
+	}
+}
+document.getElementById("arrow_left").addEventListener('click', function() {
+	if(i > 0) {
+		dot[i].classList.remove("dot_selected");
+		i--;
+		sliderImg(i);
+	}
+	else {
+		dot[i].classList.remove("dot_selected");
+		i = slides.length -1;
+		sliderImg(i);
+	}
+});
+document.getElementById("arrow_right").addEventListener('click', function() {
+	if(i < (slides.length -1)) {
+		dot[i].classList.remove("dot_selected");
+		i++;
+		sliderImg(i);
+	}
+	else {
+		dot[i].classList.remove("dot_selected");
+		i = 0;
+		sliderImg(i);
 
-const arrowLeft = document.getElementById("arrow_left");
-arrowLeft.addEventListener('click', function() {
-	alert("Clique Arrow Left");
+	}
 });
-const arrowRight = document.getElementById("arrow_right");
-arrowRight.addEventListener('click', function() {
-	alert("Clique Arrow Right");
-});
+
+
